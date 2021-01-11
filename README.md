821139e8e757c81579e70dcf243a7414405c56e7
luzhuokun <332770530@qq.com>
2021-01-11 17:43:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### Usage
+
+```
+// webpack.config.js
+const UploadfileWebpackPlugin = require('uploadfile-webpack-plugin')
+plugins: [
+  new UploadfileWebpackPlugin({
+    host: 'host',
+    port: 'port',
+    username: 'username',
+    password: 'password',
+    localpath: 'localpath',
+    remotepath: 'remotepath'
+  })
+]
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const Client = new require('ssh2-sftp-client')
+const sftp = new Client()
+
+const PLUGIN_NAME = 'UploadfileWebpackPlugin'
+
+class UploadfileWebpackPlugin {
+  constructor(options = {
+    host: '',
+    port: '',
+    username: '',
+    password: '',
+    localpath: '',
+    remotepath: ''
+  }) {
+    this.options = options
+  }
+  async execute(compilation) {
+    await sftp.connect({
+      host: this.options.host,
+      port: this.options.port,
+      username: this.options.username,
+      password: this.options.password
+    }).then(() => {
+      return sftp.put(this.options.localpath, this.options.remotepath)
+    }).then(res => {
+      console.log(res)
+      if (fs.existsSync(this.options.localpath)) fs.unlinkSync(this.options.localpath)
+      return sftp.end()
+    })
+    console.log(chalk.green('uploadfile success'))
+  }
+  apply(compiler) {
+    compiler.hooks.done.tapPromise(PLUGIN_NAME, execute)
+  }
+}
+
+module.exports = UploadfileWebpackPlugin
