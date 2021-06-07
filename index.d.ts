873ac0178a14abae0e3fa6e8449f44bab84514b1
luzhuokun <332770530@qq.com>
2021-06-07 14:46:09 +0800

    加入描述文件

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+interface Options {
+  host: string;
+  port: string;
+  username: string;
+  password: string;
+  localpath: string;
+  remotepath: string;
+  finishUpload?: Function;
+}
+
+declare class UploadfileWebpackPlugin {
+  constructor(options: Options)
+  execute(compilation): void
+  apply(compiler): void
+}
--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 const chalk = require('chalk')
 const Client = new require('ssh2-sftp-client')
 const sftp = new Client()
-
-const PLUGIN_NAME = 'UploadfileWebpackPlugin'
-
 class UploadfileWebpackPlugin {
-  constructor(options = {
-    host: '',
-    port: '',
-    username: '',
-    password: '',
-    localpath: '',
-    remotepath: ''
-  }) {
+  constructor(options) {
     this.options = options
   }
   async execute(compilation) {
-    await sftp.connect({
-      host: this.options.host,
-      port: this.options.port,
-      username: this.options.username,
-      password: this.options.password
-    }).then(() => {
-      return sftp.put(this.options.localpath, this.options.remotepath)
-    }).then(async res => {
-      if (this.options.finishUpload) await this.options.finishUpload(res)
-      return sftp.end()
-    })
-    console.log(chalk.green('uploadfile success'))
+    const { host, port, username, password, localpath, remotepath, finishUpload } = this.options
+    await sftp.connect({ host, port, username, password })
+    try {
+      const res = await sftp.put(localpath, remotepath)
+      await finishUpload(res)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      await sftp.end()
+      console.log(chalk.green(`文件上传服务器成功！`))
+    }
   }
   apply(compiler) {
-    compiler.hooks.done.tapPromise(PLUGIN_NAME, this.execute.bind(this))
+    compiler.hooks.done.tapPromise(this.__pluginName, this.execute.bind(this))
   }
 }
 
